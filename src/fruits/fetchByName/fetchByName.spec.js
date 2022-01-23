4c58522c27ac36300142e4d36d333b5e17c28fd8
mrbrunelli <matheus.brunelli@gmail.com>
2022-01-23 12:37:36 -0300

    test: ensure fetchByName returns fruit details if exists

--- src/fruits/fetchByName/fetchByName.spec.js
+++ src/fruits/fetchByName/fetchByName.spec.js
@@ -11,8 +11,27 @@ describe('fetchByName', () => {
   test('should calls fetchByName with correctly params', async () => {
     const { sut } = makeSut()
     const spy = jest.spyOn(fruits, 'fetchByName')
-    sut.fetchByName('valid fruit name')
+    await sut.fetchByName('valid fruit name')
     expect(spy).toHaveBeenCalledTimes(1)
     expect(spy).toHaveBeenCalledWith('valid fruit name')
+  })
+
+  test('should return fruit details if fruit exists', async () => {
+    const { sut } = makeSut()
+    const result = await sut.fetchByName('valid fruit name')
+    expect(result).toEqual({
+      genus: 'valid genus',
+      name: 'valid fruit name',
+      id: 1,
+      family: 'valid family',
+      order: 'valid order',
+      nutritions: {
+        carbohydrates: 1,
+        protein: 1,
+        fat: 1,
+        calories: 1,
+        sugar: 1,
+      },
+    })
   })
 })
