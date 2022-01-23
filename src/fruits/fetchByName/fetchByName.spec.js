969c14308867d65c2728fbc8f5f745609a5f58cc
mrbrunelli <matheus.brunelli@gmail.com>
2022-01-23 12:22:46 -0300

    refactor: change implementation responsability to makeSut

--- src/fruits/fetchByName/fetchByName.spec.js
+++ src/fruits/fetchByName/fetchByName.spec.js
@@ -1,9 +1,17 @@
 const fruits = require('./fetchByName')
 
+const makeSut = () => {
+  const sut = fruits
+  return {
+    sut,
+  }
+}
+
 describe('fetchByName', () => {
   test('should calls fetchByName with correctly params', async () => {
+    const { sut } = makeSut()
     const spy = jest.spyOn(fruits, 'fetchByName')
-    fruits.fetchByName('banana')
+    sut.fetchByName('banana')
     expect(spy).toHaveBeenCalledWith('banana')
   })
 })
