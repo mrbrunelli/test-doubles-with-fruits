2b9362ebab1365c2ea7f9d090a6c6d7e7563a39e
mrbrunelli <matheus.brunelli@gmail.com>
2022-01-23 12:37:18 -0300

    feat: ensure fetchByName returns fruit details if exists

--- src/fruits/fetchByName/fetchByName.js
+++ src/fruits/fetchByName/fetchByName.js
@@ -1,3 +1,16 @@
 module.exports.fetchByName = async (name) => {
-  return name
+  return Promise.resolve({
+    genus: 'valid genus',
+    name,
+    id: 1,
+    family: 'valid family',
+    order: 'valid order',
+    nutritions: {
+      carbohydrates: 1,
+      protein: 1,
+      fat: 1,
+      calories: 1,
+      sugar: 1,
+    },
+  })
 }
