135c6423f57e561ee5e6aa82248680235bcbc9bd
mrbrunelli <matheus.brunelli@gmail.com>
2022-01-29 12:25:24 -0300

    refactor: change module function to class

--- src/fruits/fetchByName/fetchByName.js
+++ src/fruits/fetchByName/fetchByName.js
@@ -1,16 +1,12 @@
-module.exports.fetchByName = async (name) => {
-  return Promise.resolve({
-    genus: 'valid genus',
-    name,
-    id: 1,
-    family: 'valid family',
-    order: 'valid order',
-    nutritions: {
-      carbohydrates: 1,
-      protein: 1,
-      fat: 1,
-      calories: 1,
-      sugar: 1,
-    },
-  })
+class FetchByName {
+  constructor({ httpService }) {
+    this.http = httpService
+  }
+
+  async handle(name) {
+    const response = await this.http.get(`https://www.fruityvice.com/api/fruit/${name}`)
+    return response.data
+  }
 }
+
+module.exports = FetchByName
--- src/fruits/fetchByName/fetchByName.spec.js
+++ src/fruits/fetchByName/fetchByName.spec.js
@@ -1,24 +1,50 @@
-const fruits = require('./fetchByName')
+const FetchByName = require('./fetchByName')
+
+const makeHttpService = () => {
+  class HttpServiceStub {
+    get() {
+      return {
+        data: {
+          genus: 'valid genus',
+          name: 'valid fruit name',
+          id: 1,
+          family: 'valid family',
+          order: 'valid order',
+          nutritions: {
+            carbohydrates: 1,
+            protein: 1,
+            fat: 1,
+            calories: 1,
+            sugar: 1,
+          },
+        },
+      }
+    }
+  }
+  return new HttpServiceStub()
+}
 
 const makeSut = () => {
-  const sut = fruits
+  const httpServiceStub = makeHttpService()
+  const sut = new FetchByName({ httpService: httpServiceStub })
   return {
     sut,
+    httpServiceStub,
   }
 }
 
-describe('fetchByName', () => {
-  test('should calls fetchByName with correctly params', async () => {
+describe('FetchByName', () => {
+  test('should calls handle method with correctly params', async () => {
     const { sut } = makeSut()
-    const spy = jest.spyOn(fruits, 'fetchByName')
-    await sut.fetchByName('valid fruit name')
+    const spy = jest.spyOn(sut, 'handle')
+    await sut.handle('valid fruit name')
     expect(spy).toHaveBeenCalledTimes(1)
     expect(spy).toHaveBeenCalledWith('valid fruit name')
   })
 
   test('should return fruit details if fruit exists', async () => {
     const { sut } = makeSut()
-    const result = await sut.fetchByName('valid fruit name')
+    const result = await sut.handle('valid fruit name')
     expect(result).toEqual({
       genus: 'valid genus',
       name: 'valid fruit name',
