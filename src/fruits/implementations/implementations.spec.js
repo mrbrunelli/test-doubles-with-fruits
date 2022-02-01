69be292c00df67eb60c3080bdb3b7b726616087d
mrbrunelli <matheus.brunelli@gmail.com>
2022-01-31 21:44:51 -0300

    test: ensure FetchAll has implemented  with correctly dependencies

--- src/fruits/implementations/implementations.spec.js
+++ src/fruits/implementations/implementations.spec.js
@@ -1,4 +1,4 @@
-const { makeFetchByName } = require('./implementations')
+const { makeFetchByName, makeFetchAll } = require('./implementations')
 const { default: axios } = require('axios')
 
 const baseUrl = 'https://www.fruityvice.com/api/fruit'
@@ -6,6 +6,12 @@ const baseUrl = 'https://www.fruityvice.com/api/fruit'
 describe('makeFetchByName', () => {
   test('should implements FetchByName with correctly dependencies', async () => {
     const sut = makeFetchByName()
+    expect(sut.http).toEqual(axios)
+    expect(sut.baseUrl).toBe(baseUrl)
+  })
+
+  test('should implements FetchAll with correctly dependencies', async () => {
+    const sut = makeFetchAll()
     expect(sut.http).toEqual(axios)
     expect(sut.baseUrl).toBe(baseUrl)
   })
