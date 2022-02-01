4e3f95a2b033dba19a74586ed2ba165c3a4277d2
mrbrunelli <matheus.brunelli@gmail.com>
2022-01-31 21:45:14 -0300

    feat: add all services examples

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-const { makeFetchByName } = require('./fruits/implementations')
+const { makeFetchByName, makeFetchAll } = require('./fruits/implementations')
 
 const main = async () => {
   const fetchFruitByName = makeFetchByName()
-  const response = await fetchFruitByName.handle('banana')
-  console.log(response)
+  const fetchAllFruits = makeFetchAll()
+
+  const result = await Promise.all([
+    fetchFruitByName.handle('banana'),
+    fetchFruitByName.handle('apple'),
+    fetchAllFruits.handle(),
+  ])
+
+  console.log({
+    ...result,
+  })
 }
 
 main()
